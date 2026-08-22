f1bb78d0780656038b1aa84ef177511b89047278
67160191 <67160191@go.buu.ac.th>
2026-08-22 14:53:32 +0700

    fix: update database charset to utf8mb4 in connection pool configuration

--- db.js
+++ db.js
@@ -8,7 +8,7 @@ const pool = mysql.createPool({
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
-    charset: 'UTF8_GENARAL_CI',
+    charset: 'utf8mb4',
 });
 
 module.exports = pool;
